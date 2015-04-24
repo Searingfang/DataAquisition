3f9681be07cc5a4507bd7932a129b5fd38f0357d
Searingfang <tmatsuz@calstatela.edu>
2015-04-24 10:06:47 -0700

    Working on daily motion api

--- dataAquisition.py
+++ dataAquisition.py
@@ -4,22 +4,30 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import json, requests
-
-url = "https://api.dailymotion.com/videos?fields=title,views_total,&sort=recent&limit=100"
-r = requests.get(url)
-
-struc = r.json()
-
+import requests
 f = open('data.txt', 'w')
-for x in struc[u'list']:
-    f.write(str(x) + '\n')
-
-url = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&key=AIzaSyDMg-eb-hHji1WEF_H_je1SXSt9HsMeofU"
-r = requests.get(url)
-
+limit = 1
+url = "https://api.dailymotion.com/videos?fields=created_time,title,views_total,&sort=visited&limit=100&page="
+r = requests.get(url + str(limit))
 struc = r.json()
-print struc.keys()
-f = open('data2.txt', 'w')
-for x in struc[u'items']:
-    f.write(str(x) + '\n')
+
+while struc[u'has_more'] and limit < 10:
+    for x in struc[u'list']:
+        f.write(x[u'title'].encode('utf-8') + '\n')
+        f.write(str(x[u'views_total']) + '\n')
+        f.write(str(x[u'created_time']) + '\n\n')
+    limit += 1
+    print limit
+#    r = requests.get(url + limit)
+#    struc = r.json()
+    
+
+
+#url = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&key=AIzaSyDMg-eb-hHji1WEF_H_je1SXSt9HsMeofU"
+#r = requests.get(url)
+#
+#struc = r.json()
+#print struc.keys()
+#f = open('data2.txt', 'w')
+#for x in struc[u'items']:
+#    f.write(str(x) + '\n')
