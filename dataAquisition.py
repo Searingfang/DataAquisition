7157b1995ca2a8b6a9e4ab5e32e2f89057cf83b6
Searingfang <tmatsuz@calstatela.edu>
2015-04-25 17:08:27 -0700

    Have both of the things

--- dataAquisition.py
+++ dataAquisition.py
@@ -10,24 +10,36 @@ limit = 1
 url = "https://api.dailymotion.com/videos?fields=created_time,title,views_total,&sort=visited&limit=100&page="
 r = requests.get(url + str(limit))
 struc = r.json()
-
+for x in struc[u'list']:
+    f.write(x[u'title'].encode('utf-8') + '\n')
+    f.write('total views: ' + str(x[u'views_total']) + '\n')
+    f.write('created time: ' + str(x[u'created_time']) + '\n\n')
+    
 while struc[u'has_more'] and limit < 10:
+    limit += 1
+    r = requests.get(url + str(limit))
+    struc = r.json()
     for x in struc[u'list']:
         f.write(x[u'title'].encode('utf-8') + '\n')
-        f.write(str(x[u'views_total']) + '\n')
-        f.write(str(x[u'created_time']) + '\n\n')
-    limit += 1
-    print limit
-#    r = requests.get(url + limit)
-#    struc = r.json()
+        f.write('total views: ' + str(x[u'views_total']) + '\n')
+        f.write('created time: ' + str(x[u'created_time']) + '\n\n')    
+
+
+f = open('data2.txt', 'w')
+limit = 1
+url = "https://www.googleapis.com/youtube/v3/videos?part=statistics%2Csnippet&chart=mostPopular&key=AIzaSyDMg-eb-hHji1WEF_H_je1SXSt9HsMeofU"
+r = requests.get(url)
+struc = r.json()
+
+for x in struc[u'items']:
+    f.write(x[u'snippet'][u'title'].encode('utf-8') + '\n')
+    f.write('total views: ' + str(x[u'statistics'][u'viewCount']) + '\n')
+    f.write('created time: ' + str(x[u'snippet'][u'publishedAt']) + '\n')
     
-
-
-#url = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&key=AIzaSyDMg-eb-hHji1WEF_H_je1SXSt9HsMeofU"
-#r = requests.get(url)
-#
-#struc = r.json()
-#print struc.keys()
-#f = open('data2.txt', 'w')
-#for x in struc[u'items']:
-#    f.write(str(x) + '\n')
+while u'nextPageToken' in struc and limit < 10:
+    r = requests.get(url + '&pageToken=' + struc[u'nextPageToken'])
+    struc = r.json()
+    for x in struc[u'items']:
+        f.write(x[u'snippet'][u'title'].encode('utf-8') + '\n')
+        f.write('total views: ' + str(x[u'statistics'][u'viewCount']) + '\n')
+        f.write('created time: ' + str(x[u'snippet'][u'publishedAt']) + '\n')
